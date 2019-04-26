82fb8c6010d5eb25f31d4bbdf1435555b9fd6dfc
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-04-26 12:47:08 +0200

    No ingredients message

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,15 +3,23 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    ingredientKey => {
+  //transforming objectinto array and display
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return (
           <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
         );
       });
-    }
-  );
+    })
+    //flattening array (array.length i a number of ingredients)
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Add ingriedients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
-  state= {
+  state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
-  }
+  };
   render() {
     return (
       <React.Fragment>
