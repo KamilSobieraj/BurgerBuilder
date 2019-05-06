d26a169d58175cb62d7babefb108e9e0ef98e851
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-05-06 12:12:49 +0200

    Toolbar + Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => (
   <React.Fragment>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
+    <div>SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </React.Fragment>
 );
