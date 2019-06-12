a66c90868d9ace57d0b44b38ebdefa87fd9489e3
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-06-12 12:08:27 +0200

    ContactData + Input: - own Input component created

--- src/containers/Chekcout/ContactData/ContactData.js
+++ src/containers/Chekcout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./ContactData.module.css";
 import Button from "../../../components/UI/Button/Button";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
@@ -21,26 +22,26 @@ class ContactData extends Component {
       <div className={classes.ContactData}>
         <h4>Fill The Form With Your Contact Data</h4>
         <form>
-          <input
-            className={classes.Input}
+          <Input
+            inputtype="input"
             type="text"
             name="name"
             placeholder="Your Name"
           />
-          <input
-            className={classes.Input}
+          <Input
+            inputtype="input"
             type="email"
             name="email"
             placeholder="Your E-mail"
           />
-          <input
-            className={classes.Input}
+          <Input
+            inputtype="input"
             type="text"
             name="street"
             placeholder="Street"
           />
-          <input
-            className={classes.Input}
+          <Input
+            inputtype="input"
             type="text"
             name="postalCode"
             placeholder="Postal Code"
